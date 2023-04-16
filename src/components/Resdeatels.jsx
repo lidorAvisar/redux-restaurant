eee2132017a383b02b213d7ba9f648e283f8972d
lidor <avisar0402@gmail.com>
2023-04-16 12:12:24 +0300

    p

--- src/components/Resdeatels.jsx
+++ src/components/Resdeatels.jsx
@@ -4,8 +4,8 @@ import './style.css'
 export default function Resdeatels() {
   return (
     <div>
-       <div className='navBarColor container'>
-        <div className='bg-info mt-5 pt-5' style={{ height: '180px' }}><span className='fs-1'>{}</span><br />
+      <div className='navBarColor container'>
+        <div className='bg-info mt-5 pt-5' style={{ height: '180px' }}><span className='fs-1'>{ }</span><br />
         </div>
       </div>
     </div>
